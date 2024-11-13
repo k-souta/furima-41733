83e72813b3016ffb01945781ff826b3e39e00523
k-souta <souten8018@ezweb.ne.jp>
2024-11-13 21:14:55 +0900

    Basic認証 commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basci_auth
+
+  private
+
+  def basci_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
+  end
 end
