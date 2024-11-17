c0f43c03da29c94be1aeaadb9ad5b95331f95f0d
k-souta <souten8018@ezweb.ne.jp>
2024-11-17 20:53:30 +0900

    ユーザー管理機能　バリデーション設定　commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :itmes
+
+  validates :nickname, presence: true
+  validates :birth, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :last_name
+    validates :name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ } do
+    validates :last_name_katakana
+    validates :name_katakana
+  end
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates_format_of :password, with: PASSWORD_REGEX
 end
