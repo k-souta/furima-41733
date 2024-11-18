59c982b895c330b6e18ccd1819e670c6345b47b8
k-souta <souten8018@ezweb.ne.jp>
2024-11-18 22:09:06 +0900

    商品出品機能　Active Storage導入 commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,11 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  private
+
+  def message_params
+    params.require(:item).permit(:item_name, :category_id, :cost_id, :item_explanation, :prefecture_id, :hidzuke_id, :price,
+                                 :image).merge(user_id: current_user.id)
+  end
 end
