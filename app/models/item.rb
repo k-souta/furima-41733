3ab3a16cdc25692fd685d9ebb4a8494976ab1c06
k-souta <souten8018@ezweb.ne.jp>
2024-11-19 19:41:22 +0900

    商品出品機能　日付と状態　commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,8 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :cost_load
   belongs_to :category
+  belongs_to :cost
+  belongs_to :hidzuke
 
-  validates :cost_load_id, :category, numericality: { other_than: 1, message: "can't be blank" }
+  validates :cost_load_id, :category_id, :cost_id, :hidzuke_id, numericality: { other_than: 1, message: "can't be blank" }
 end
