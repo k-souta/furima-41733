f26457753f45cdc244021fd07d79a9d8aaf2cf4e
k-souta <souten8018@ezweb.ne.jp>
2024-11-19 18:28:53 +0900

    商品出品機能 カテゴリーと負担 commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
+  belongs_to :cost_load
+  belongs_to :category
+
+  validates :cost_load_id, :category, numericality: { other_than: 1, message: "can't be blank" }
 end
