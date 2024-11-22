98a778fc20e21ad83b9f8e2d632b6cf0ecd2809b
k-souta <souten8018@ezweb.ne.jp>
2024-11-22 20:32:58 +0900

    商品情報編集　修正1  commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :correct_item, only: [:edit]
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.all
     @items = Item.order(created_at: :desc)
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -48,5 +46,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :category_id, :cost_id, :item_explanation, :prefecture_id, :hidzuke_id, :price, :cost_load_id,
                                  :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
