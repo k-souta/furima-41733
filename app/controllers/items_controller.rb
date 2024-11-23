0389e4484b79889949201a9c1429abc4adadcac5
k-souta <souten8018@ezweb.ne.jp>
2024-11-23 16:49:30 +0900

    商品削除機能 commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,6 +40,12 @@ class ItemsController < ApplicationController
     redirect_to '/'
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to '/'
+  end
+
   private
 
   def item_params
