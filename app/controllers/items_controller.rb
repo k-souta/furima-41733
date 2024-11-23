2b2d672c82ffd0c31282b35083fcf68788cff7ba
k-souta <souten8018@ezweb.ne.jp>
2024-11-23 17:44:02 +0900

    商品削除機能　修正1　commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :correct_item, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :correct_item, only: [:edit, :updata, :destroy]
 
   def index
@@ -40,17 +40,17 @@ class ItemsController < ApplicationController
     redirect_to '/'
   end
 
-  def correct_item
-    return if @item.user.id == current_user.id
-
-    redirect_to '/'
-  end
-
   private
 
   def item_params
     params.require(:item).permit(:item_name, :category_id, :cost_id, :item_explanation, :prefecture_id, :hidzuke_id, :price, :cost_load_id,
                                  :image).merge(user_id: current_user.id)
+  end
+
+  def correct_item
+    return if @item.user.id == current_user.id
+
+    redirect_to '/'
   end
 
   def set_item
